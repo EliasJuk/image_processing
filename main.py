22992c33cedb2c7251f7e334f69b19472ca4cff0
EliasJuk <eliasp.juk@hotmail.com>
2021-03-25 21:43:37 -0300

    deletar imagens

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from PIL import Image
 
 # Layout
 first_column = [
-  [sg.Input(disabled=True, enable_events=True, key='-FOLDER-', size=(32,1)), sg.FileBrowse()],
-  [sg.Listbox(values=fnames, enable_events=True, size=(40, 20), key="-FILE LIST-")]
+  [sg.Input(disabled=True, enable_events=True, key='-FOLDER-', size=(33,1)), sg.FileBrowse('Browse')],
+  [sg.Button('DELETAR', size=(17, 1)), sg.Button('ATUALIZAR', size=(17, 1))],
+  [sg.Listbox(values=fnames, enable_events=True, size=(40, 20), key="-FILE LIST-")],
 ]
 
 second_column = [
@@ -25,7 +26,7 @@ window = sg.Window('image_processing', layout)
 # Eventos
 while True:
   event, values = window.Read()
-
+  
   if event == '-FOLDER-':
     path_image = values['-FOLDER-']
 
@@ -43,40 +44,71 @@ while True:
         try:
           os.remove(input_folder+image_name)
         except:
-          sg.Popup('Não foi possivel deletar a imagem!!')
+          sg.Popup('Não foi possivel deletar a imagem')
         
-        #ATUALIZA IMAGEM
-        window["-IMAGE-"].update(input_folder+new_image_name)
+        #GERAR IMAGEM MINIATURA E ATUALIZA IMAGEM
+        gerar_miniatura(input_folder+new_image_name)
+        window["-IMAGE-"].update(input_folder+'temp/'+new_image_name)
         
         arquivos = listar_images()
+        fnames = arquivos
         window["-FILE LIST-"].update(arquivos)
       except:
-        sg.Popup('Ocorreu um erro ao carregar a imagem!!')
+        sg.Popup('Ocorreu um erro ao carregar a imagem')
     elif path_image.endswith(".png") == True:
       shutil.copy(path_image,input_folder)
-      image_name = os.path.basename(path_image)
+      new_image_name = os.path.basename(path_image)
       try:
-        window["-IMAGE-"].update(input_folder+image_name)
+        #GERAR IMAGEM MINIATURA E ATUALIZA IMAGEM
+        gerar_miniatura(input_folder+new_image_name)
+        window["-IMAGE-"].update(input_folder+'temp/'+new_image_name)
 
         arquivos = listar_images()
+        fnames = arquivos
         window["-FILE LIST-"].update(arquivos)
       except:
         #CASO A IMAGEM TENHA FORMATO DIFERENTE E TENHA SIDO RENOMEADA
         #CONVERTE IMAGEM PARA PNG PARA EVITAR PARA EVITAR POSSIVEIS ERROS
-        Image.open(input_folder+image_name).save(input_folder+image_name)
-        window["-IMAGE-"].update(input_folder+image_name)
+        Image.open(input_folder+new_image_name).save(input_folder+new_image_name)
+        #GERAR IMAGEM MINIATURA E ATUALIZA IMAGEM
+        gerar_miniatura(input_folder+new_image_name)
+        window["-IMAGE-"].update(input_folder+'temp/'+new_image_name)
 
         arquivos = listar_images()
+        fnames = arquivos
         window["-FILE LIST-"].update(arquivos)
     else:
-      sg.Popup('Ocorreu um erro ao carregar a imagem!!!')
+      sg.Popup('Ocorreu um erro ao carregar a imagem') 
 
   #ATULIZAR IMAGEM AO SELECIONAR NA LISTBOX
   if event == "-FILE LIST-":
-    filename = os.path.join(
-      values["-FILE LIST-"][0]
-    )
-    window["-IMAGE-"].update(filename='input/'+filename)
+    try:
+      filename = os.path.join(
+        values["-FILE LIST-"][0]
+      )
+      window["-IMAGE-"].update(filename='input/temp/'+filename)
+    except:
+      sg.popup('Adicione uma imagem')
+
+  if event == 'DELETAR':
+    try:
+      arquivos = listar_images()
+      fnames = arquivos
+      window["-FILE LIST-"].update(arquivos)
+
+      item_selected = values['-FILE LIST-'][0]
+      
+      fnames.remove(item_selected)
+      window.FindElement('-FILE LIST-').Update(values=fnames)
+      os.remove(input_folder+item_selected)
+      os.remove(input_folder+'temp/'+item_selected)
+    except:
+      sg.Popup('Não foi selecionado nada para deletar')
+
+  if event == 'ATUALIZAR':
+    arquivos = listar_images()
+    fnames = arquivos
+    window.FindElement('-FILE LIST-').Update(values=fnames)
 
   if event == sg.WIN_CLOSED or event == 'Quit':
     break
